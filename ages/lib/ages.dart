52372b3800f5361487df3be29f992de83ff3219d
Dmitry Zhuravlev <d.zhuravlev@fun-box.ru>
2023-09-07 21:18:40 +0300

    home task № 1

---
+++ ages/lib/ages.dart
@@ -0,0 +1,67 @@
+enum Countries { brazil, russia, turkish, spain, japan }
+
+class Territory {
+  int areaInHectare;
+  List<String> crops;
+  List<AgriculturalMachinery> machineries;
+
+  Territory(
+    this.areaInHectare,
+    this.crops,
+    this.machineries,
+  );
+}
+
+class AgriculturalMachinery {
+  final String id;
+  final DateTime releaseDate;
+
+  AgriculturalMachinery(
+    this.id,
+    this.releaseDate,
+  );
+
+  // Переопределяем оператор "==", что бы сравнивать объекты по значению
+  @override
+  bool operator ==(Object? other) {
+    if (other is! AgriculturalMachinery) return false;
+    if (other.id == id && other.releaseDate == releaseDate) return true;
+
+    return false;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ releaseDate.hashCode;
+}
+
+List<AgriculturalMachinery> receiveCars(
+    Map<Countries, List<Territory>> data, List<AgriculturalMachinery> storage) {
+  for (var v in data.values) {
+    for (var value in v) {
+      for (var el in value.machineries) {
+        storage.add(el);
+      }
+    }
+  }
+  return storage;
+}
+
+List<AgriculturalMachinery> uniqCars(List<AgriculturalMachinery> cars) {
+  for (var i = 1; i < cars.length; i++) {
+    if (cars[i - 1] == cars[i]) cars.removeAt(i);
+  }
+  return cars;
+}
+
+middleReleaseDates(
+    List<AgriculturalMachinery> cars, int currentYear, int part) {
+  var sumAges = cars.fold<double>(
+      0, (state, car) => (state + currentYear - car.releaseDate.year));
+  var sumPartAges = cars.take(part).fold<double>(
+      0, (state, car) => (state + currentYear - car.releaseDate.year));
+  var middleAgeAllCars = (sumAges / cars.length).round();
+  var middleAgeOldCars = (sumPartAges / part).round();
+
+  print(
+      'Средний возраст техники $middleAgeAllCars лет, средний возраст самой старой техники $middleAgeOldCars года');
+}
